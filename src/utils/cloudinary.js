380a51eaacb769ef2b0dbdd678ca12bb0e3f3424
prasoon badola <prasoonbadola@gmail.com>
2024-07-07 02:10:49 +0530

    added file upload functionality

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary = async (localfilepath) => {
+    try {
+        if (!localfilepath) return null
+        //upload the file on cloudinary
+
+        const response = await cloudinary.uploader.upload(localfilepath, {
+            resource_type: "auto"
+        })
+
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localfilepath) // remove the locally saved temprory filed as the upload operation got failed
+        return null;
+    }
+}
+
+
+export { uploadOnCloudinary }
+
